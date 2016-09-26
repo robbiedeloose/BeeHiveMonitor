75c8c3ac0c3acfb941ffb1f6b1def1dbba7df64d
robbiedeloose <robbie@banded.be>
2016-09-26 23:53:14 +0200

    ds sensor per adress

--- BeeHiveMonitor.ino
+++ BeeHiveMonitor.ino
@@ -1,7 +1,7 @@
 /*
 
-genuino mkr1000n --> wifi101 library
-dht22 temp an humidity sensor
+  genuino mkr1000n --> wifi101 library
+  dht22 temp an humidity sensor
 
 */
 
@@ -30,12 +30,12 @@ dht22 temp an humidity sensor
 #include <SPI.h>
 #include <WiFi101.h>
 
-//char ssid[] = "telenet-replica"; //  your network SSID (name)
-//char pass[] = "newyork20newyork15";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "telenet-replica"; //  your network SSID (name)
+char pass[] = "newyork20newyork15";    // your network password (use for WPA, or use as key for WEP)
 
 // zee
-char ssid[] = "telenet-224D7"; //  your network SSID (name)
-char pass[] = "Zcmssfbu3dvj";    // your network password (use for WPA, or use as key for WEP)
+//char ssid[] = "telenet-224D7"; //  your network SSID (name)
+//char pass[] = "Zcmssfbu3dvj";    // your network password (use for WPA, or use as key for WEP)
 
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
@@ -66,8 +66,29 @@ WiFiClient client;
 // http://www.pjrc.com/teensy/td_libs_OneWire.html
 // The DallasTemperature library can do all this work for you!
 // http://milesburton.com/Dallas_Temperature_Control_Library
+
+/* YourDuino TEST: Temperature Sensor DS18B20
+    V1.2 08-26-2016
+  - Connect cable to Arduino Digital I/O Pin 2
+  terry@yourduino.com */
+
+/*-----( Import needed libraries )-----*/
 #include <OneWire.h>
-OneWire  ds(3);  // on pin 3 (a 4.7K resistor is necessary)
+#include <DallasTemperature.h>
+
+/*-----( Declare Constants )-----*/
+#define ONE_WIRE_BUS 3 /*-(Connect to Pin 2 )-*/
+
+/*-----( Declare objects )-----*/
+/* Set up a oneWire instance to communicate with any OneWire device*/
+OneWire ourWire(ONE_WIRE_BUS);
+
+/* Tell Dallas Temperature Library to use oneWire Library */
+DallasTemperature sensors(&ourWire);
+
+/*-----( Declare Variables )-----*/
+
+// on pin 3 (a 4.7K resistor is necessary)
 
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
@@ -136,6 +157,10 @@ void setup() {
     client.println("Connection: close");
     client.println();
   }
+
+/*-( Start up the DallasTemperature library )-*/
+sensors.begin();
+  
 }
 
 void loop() {
@@ -245,103 +270,27 @@ void printWifiStatus() {
 
 
 void readSensors () {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-  float celsius, fahrenheit;
 
-  if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
-    delay(250);
-    return;
-  }
-
-  Serial.print("ROM =");
-  for ( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-    Serial.println("CRC is not valid!");
-    return;
-  }
   Serial.println();
+Serial.print("Requesting temperature...");
+sensors.requestTemperatures(); // Send the command to get temperatures
+Serial.println("DONE");
 
-  // the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  }
+Serial.print("Device 1 (index 0) = ");
+Serial.print(sensors.getTempCByIndex(0));
+Serial.println(" Degrees C");
+Serial.print("Device 1 (index 0) = ");
+Serial.print(sensors.getTempFByIndex(0));
+Serial.println(" Degrees F");
 
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+Serial.print("Device 1 (index 1) = ");
+Serial.print(sensors.getTempCByIndex(1));
+Serial.println(" Degrees C");
+Serial.print("Device 1 (index 1) = ");
+Serial.print(sensors.getTempFByIndex(1));
+Serial.println(" Degrees F");
 
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);         // Read Scratchpad
-
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-  celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-  Serial.print("  Temperature = ");
-  Serial.print(celsius);
-  Serial.print(" Celsius, ");
-  Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit");
-
-
+  
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Wait a few seconds between measurements.
